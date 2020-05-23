9e7373b9da62865cb31047e0a915caf8b7e0048a
acamalvarez <acam@outlook.com>
2020-05-23 11:31:11 -0500

    final changes

---
+++ functions.py
@@ -0,0 +1,147 @@
+
+import numpy as np
+import pandas as pd
+from constants import Constants
+from scipy.optimize import minimize
+
+def Gibbs(x, p):
+    '''
+    Arguments:
+    T -- input temperature
+    P -- input pressure
+    x -- list of moles of compound in the following order CH4, CO2, H2O, O2, CO, H2, He, C
+
+    Returns:
+    Gibbs free energy
+    '''
+    # converts the list of moles of each compound into an numpy array
+    nj = np.array([x])
+
+    # Defines the T and p, from the parameters list
+    T = p[0]
+    P = p[1]
+
+    # Vector to find the Delta free energy at a specific temperature
+    T_vector = np.array([[1], [T], [T**2], [T**3], [T**4]])
+
+    Enj = np.sum(nj) # total moles
+    y_j = nj / Enj
+
+    # vector for the coefficients of fugacity
+    phi = np.ones((1, 7))
+
+    # Gibbs free energy of formation, kJ/mol
+    Gj0 = np.dot(Constants.DelG_f, T_vector)
+
+    first = np.sum(nj[0][0:-1] * Gj0.T[0][0:-1])
+    second = Constants.R * T * np.sum(nj[0][0:-1] * np.log(y_j[0][0:-1] * phi[0][0:-1] * P / Constants.P0))
+    third = nj[0][-1] * Gj0.T[0][-1]
+
+    G = first + second + third
+
+    return G
+
+#  CH4  CO2  H2O  O2    CO   H2   He   C
+def calculate_mol(mol_0, T, P):
+    '''
+    calculates the moles at the equilibrium
+    Arguments:
+    -- moles_0: initial moles in this order: 
+        CH4, CO2, H2O, O2, CO, H2, C
+    -- T: temperature, °C
+    -- P: pressure, atm
+    Returns:
+    -- numpy array with the moles at the equilibrium of 
+    CH4, CO2, H2O, O2, CO, H2, C
+
+    '''
+    # calculates temperature and pressure to convinient units
+    T = T + 273  # temperature, K
+    P = 1  # pressure, bar
+
+    # organizes the parameters (T and P) in a numpy array
+    parameters = np.array([T, P])
+
+    # calculates the vector of elemental balances
+    beq = np.dot(Constants.Aeq, np.array(mol_0))
+
+    # Defines the equality constraints with the elemental balance
+    eq_cons = {'type': 'eq', 'fun': lambda x: np.dot(
+        Constants.Aeq, np.array(x)) - beq}
+
+    # options for the solver of the optimization
+    options = {'ftol': 1e-6, 'disp': False, 'maxiter': 1000,
+               'eps': 1.4901161193847656e-8}
+
+    # solve the optimization problem
+    result = minimize(Gibbs, Constants.n0, args=parameters, method='SLSQP',
+                      bounds=Constants.bnds,
+                      constraints=eq_cons, options=options)
+
+    return result.x
+
+
+def find_results(mol_0, Temp, P):
+    """
+    returns a data frame with the results at each temperature of the 
+    Temp array in the following order:
+    moles of each compound; conversions of CH4, CO2, and H2O; and H2/CO ratio
+
+    Arguments:
+    -- moles_0: initial moles
+    -- Temp: array of the temperature to evaluate, C
+    -- P: pressure of the system, bar
+
+    Returns:
+    -- data frame of the results
+
+    """
+    
+    # array for storing the moles
+    mol = np.zeros((len(Temp), 7))
+
+    # array for storing the conversions
+    conversions = np.zeros((len(Temp), 7))
+
+    # for loop to calculate moles and conversions at each temperature
+    for i in np.arange(len(Temp)):
+
+        # calculates moles and storages in the moles array
+        mol[i] = calculate_mol(mol_0, Temp[i], P)
+
+        # calculates conversions and storates in the conversions array
+        conversions[i] = 100 * (mol_0 - mol[i]) / mol_0
+
+    # creates moles data frame
+    df_mol = pd.DataFrame(data=mol, index=Temp,
+                            columns=Constants.columns_mol)
+
+    # creates conversions data frame
+    df_conv = pd.DataFrame(data=conversions, index=Temp,
+                        columns=Constants.columns_conversions)
+
+    # concatenate the data frames in results data frame
+    df_results = pd.concat(
+        [df_mol, df_conv[['conv_CH4', 'conv_CO2', 'conv_H2O']]], axis=1)
+
+    # calculates H2/CO ratio and adds a column to the data frame results
+    df_results['H2/CO'] = df_results['mol_H2'] / df_results['mol_CO']
+
+    return df_results
+
+def generate_data_for_NN(mol_0, Temp, P):
+
+    finalResults = find_results(mol_0, Temp, P)[['conv_CH4', 'conv_CO2', 'conv_H2O', 'H2/CO']]
+    finalResults['CH4_0'] = mol_0[0]
+    finalResults['CO2_0'] = mol_0[1]
+    finalResults['H2O'] = mol_0[2]
+    finalResults['O2_0'] = mol_0[3]
+
+    return finalResults[['CH4_0', 'CO2_0', 'H2O', 'O2_0', 'conv_CH4', 'conv_CO2', 'conv_H2O', 'H2/CO']]
+
+
+def concat_results(moles_0, Temp, P):
+
+    allResults = pd.concat(generate_data_for_NN(mol_0, Temp, P) for mol_0 in moles_0)
+
+    return allResults
--- main_triReforming.py
+++ main_triReforming.py
@@ -1,84 +1,30 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
-from scipy.optimize import minimize
+from functions import find_results, generate_data_for_NN, concat_results
+from constants import Strings
 
-from DelG_f import Gibbs
-from constants import Constants
+plt.rcParams['axes.labelweight'] = 'bold'
 
-#  CH4  CO2  H2O  O2    CO   H2   He   C
-def found_moles(moles_0, T, P):
-
-    '''
-    calculates the moles at the equilibrium
-    Arguments:
-    -- moles_0: initial moles in this order: 
-        CH4, CO2, H2O, O2, CO, H2, He, C
-    -- T: temperature, °C
-    -- P: pressure, atm
-    Returns:
-    -- numpy array with the moles at the equilibrium of 
-    CH4, CO2, H2O, O2, CO, H2, He, C
-    
-    '''
-    # calculates temperature and pressure to convinient units
-    T = T + 273 # temperature, K
-    P = 1  # pressure, bar
-
-    # organizes the parameters (T and P) in a numpy array
-    parameters = np.array([T, P])
-
-    # calculates the vector of elemental balances
-    beq = np.dot(Constants.Aeq, np.array(moles_0))
-
-    print(beq)
-
-    # Defines the equality constraints with the elemental balance
-    eq_cons = {'type': 'eq', 'fun': lambda x: np.dot(Constants.Aeq, np.array(x)) - beq}
-
-    # eq_cons = {'type': 'eq', 'fun': lambda x: np.array([np.sum(Constants.Aeq[0] * np.array(x)) - beq[0], 
-    #                                                     np.sum(Constants.Aeq[1] * np.array(x)) - beq[1], 
-    #                                                     np.sum(Constants.Aeq[2] * np.array(x)) - beq[2], 
-    #                                                     np.sum(Constants.Aeq[3] * np.array(x)) - beq[3]])}
-
-    options = {'ftol': 1e-1, 'disp':True, 'maxiter':1000}
-
-    # solve the optimization problem
-    result = minimize(Gibbs, Constants.n0, args=parameters, method='SLSQP',
-                    bounds=Constants.bnds,
-                      constraints=eq_cons, options=options)
-
-    return result.x
-
-#          CH4, CO2, H2O, O2,  CO,  H2,  He,  C
-moles_0 = [0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 0.1, 0.0]
-# Temp = np.arange(200, 1050, 50)
-Temp = [550]
+#          CH4, CO2, H2O, O2,  CO,  H2,  C
+moles_0 = [[1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+           [1.0, 0.0, 1.0, 0.5, 0.0, 0.0, 0.0],
+           [1.0, 0.0, 1.0, 0.5, 0.0, 0.0, 0.0]]
+Temp = np.arange(200, 1050, 50)
 P = 1
 
-moles = np.zeros((len(Temp), 8))
-conversions = np.zeros((len(Temp), 8))
+# df = find_results(moles_0, Temp, P)
 
-for i in np.arange(len(Temp)):
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.plot(df[['mol_CH4', 'mol_CO2', 'mol_H2O']], '-o')
 
-    moles[i] = found_moles(moles_0, Temp[i], P)
+# ax.set_xlabel(Strings.Temperature)
+# ax.set_ylabel(Strings.mol)
+# plt.legend([Strings.mol_CH4, Strings.mol_CO2, Strings.mol_H2O])
+# plt.show()
 
-    conversions[i] = 100 * (moles_0 - moles[i]) / moles_0
+# print(generate_data_for_NN(moles_0[0], Temp, P))
 
-df_moles = pd.DataFrame(data=moles, index=Temp, 
-        columns=Constants.columns_moles)
-
-df_conv = pd.DataFrame(data=conversions, index=Temp, 
-        columns=Constants.columns_conversions)
-
-
-# print(df_conv[['conv_CH4', 'conv_CO2', 'conv_H2O']])
-print(df_moles)
-
-
-
-def create_df(moles_0, Temperatures, P):
-    pass
-
-
+print(concat_results(moles_0, Temp, P))
